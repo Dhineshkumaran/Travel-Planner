8a7e168f1862c6253a369bcd57f254e7f742446e
Dhinesh kumaran S <dhineshkumaran2004@outlook.com>
2024-10-30 19:30:09 +0530

    IP

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://13.239.183.126:4000/api/v1'
+export const BASE_URL = 'http://52.64.181.168:4000/api/v1'
 // export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
